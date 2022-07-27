945851e0fa4e1e9d602be5ac17dcdd9ebe4974a0
GuillaumeFavrot <gui.favrot@gmail.com>
2022-07-27 12:56:22 +0200

    config var fix

--- api/utils.py
+++ api/utils.py
@@ -3,6 +3,8 @@ import os
 
 connection_string = os.environ.get("MONGO_URI", 'dev default value')
 
+print(connection_string)
+
 client = MongoClient(connection_string)
 
 db = client['TaskListManager']
